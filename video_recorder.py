e53147454636bc03941ef97a8361a77db5dab55e
AbdSaid <153506053+Abd-Said@users.noreply.github.com>
2024-04-01 15:06:15 +0300

    Create video_recorder.py

---
+++ video_recorder.py
@@ -0,0 +1,32 @@
+import cv2
+from datetime import datetime
+
+video_capture = cv2.VideoCapture(0)
+
+width = int(video_capture.get(3))
+height = int(video_capture.get(4))
+
+now = datetime.now()
+timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
+fourcc = cv2.VideoWriter_fourcc(*'XVID') #choose best type for your code. MPEG , MJPG , H265 , H264 , XVID
+filename = f'Video_{timestamp}.mp4' #change the video forat .avi .mp4 etc.
+video_writer = cv2.VideoWriter(filename , fourcc, 30, (width, height))
+
+while True:
+
+    ret, frame = video_capture.read()
+
+    if not ret:
+        break
+
+    video_writer.write(frame)
+
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+video_capture.release()
+video_writer.release()
+cv2.destroyAllWindows()
